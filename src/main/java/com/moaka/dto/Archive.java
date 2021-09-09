3ee0b9cf58c5e9b66158a491027124a11d2f1dae
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2021-09-09 19:10:23 +0900

    아카이브 리스트 배포 기능

--- src/main/java/com/moaka/dto/Archive.java
+++ src/main/java/com/moaka/dto/Archive.java
@@ -10,10 +10,15 @@ import java.util.ArrayList;
 public class Archive {
     private int no;
     private String title;
+    private String description;
     private String thumbnail;
     private String privacy_type;
+    // 생성자 이름
+    private String creator_name;
+    // 생성자 고유번호
     private int user_no;
     private String regdate;
-
-    ArrayList<Section> sectionList;
+    
+    ArrayList<Section> section_list;
+    ArrayList<String> tag_list;
 }
