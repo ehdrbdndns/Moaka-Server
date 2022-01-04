dc4f7190cabfea7f276194a95593478b24719504
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2022-01-05 01:35:44 +0900

    rabbitmq 속성 변경

--- src/main/java/com/moaka/common/config/StompConfig.java
+++ src/main/java/com/moaka/common/config/StompConfig.java
@@ -1,5 +1,6 @@
 package com.moaka.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -10,11 +11,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOriginPatterns("http://*.*.*.*:3000", "http://*:3000", "http://*.*.*.*:8080", "http://*:8080") //안해도 무관
+                .setAllowedOriginPatterns("*") //안해도 무관
                 .withSockJS();
     }
 
@@ -23,8 +32,21 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
         registry.setPathMatcher(new AntPathMatcher(".")); // url을 chat/room/3 -> chat.room.3으로 참조하기 위한 설정
         registry.setApplicationDestinationPrefixes("/pub");
-
-        //registry.enableSimpleBroker("/sub");
         registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
+//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+//                .setSystemLogin(username)
+//                .setSystemPasscode(password)
+//                .setClientLogin(username)
+//                .setClientPasscode(password);
+//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+//                .setRelayPort(61613);
+//        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+//                .setRelayHost(host)
+//                .setRelayPort(61613)
+//                .setSystemLogin(username)
+//                .setSystemPasscode(password)
+//                .setClientLogin(username)
+//                .setClientPasscode(password);
+        ;
     }
 }
